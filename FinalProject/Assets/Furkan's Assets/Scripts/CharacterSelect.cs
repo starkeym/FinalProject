718b73824c5815357f09b659b9a9d2cd46db9a98
rinconcept <57409056+rinconcept@users.noreply.github.com>
2019-11-29 06:06:00 +0300

    asd

---
+++ FinalProject/Assets/Furkan's Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public void Shooter() {
+        SetupLocalPlayer.chID = 0;
+        SceneManager.LoadScene(1);
+    }
+    public void Healer()
+    {
+        SetupLocalPlayer.chID = 1;
+        SceneManager.LoadScene(1);
+    }
+    public void Tank()
+    {
+        SetupLocalPlayer.chID = 2;
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ FinalProject/Assets/Furkan's Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    public static int chID;
+    public GameObject[] Characters;
+    GameObject localGameOBJ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            CmdspawnCharacter();
+        }
+
+    }
+
+    [Command]
+    void CmdspawnCharacter() {
+        localGameOBJ = Instantiate(Characters[chID]);
+        localGameOBJ.GetComponent<SimpleMovement>().enabled = true;
+        NetworkServer.Spawn(localGameOBJ);
+    }
+}
